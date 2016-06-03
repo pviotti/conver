e9107e1a67b5b1b9d920e164b3e0ce1605d13a30
Paolo Viotti <paolo.viotti@eurecom.fr>
2016-06-03 18:34:05 +0200

    add new-old inversion and crossed read-writes tests

---
+++ src/test/scala/conver/CheckerTest.scala
@@ -0,0 +1,27 @@
+package conver
+
+import org.scalatest.FunSuite
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.HashSet
+
+class CheckerTest extends FunSuite {
+
+  test("New-old inversion") {
+    val ops = new ListBuffer[Operation]
+    ops += new Operation('a', WRITE, 1, 5, 1, new HashSet[String])
+    ops += new Operation('a', WRITE, 7, 15, 2, new HashSet[String])
+    ops += new Operation('b', READ, 4, 8, 2, new HashSet[String])
+    ops += new Operation('c', READ, 9, 12, 1, new HashSet[String])
+    Checker.checkExecution(ops)
+  }
+
+  test("Crossed concurrent read-writes") {
+    val ops = new ListBuffer[Operation]
+    ops += new Operation('a', WRITE, 1, 2, 1, new HashSet[String])
+    ops += new Operation('b', WRITE, 1, 2, 2, new HashSet[String])
+    ops += new Operation('a', READ, 4, 6, 1, new HashSet[String])
+    ops += new Operation('b', READ, 4, 6, 2, new HashSet[String])
+    Drawer.drawExecution(2, ops, 8)
+    Checker.checkExecution(ops)
+  }
+}
